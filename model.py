f415340dca1048333895f482493a3389c3a6af74
k920049 <k920049@naver.com>
2019-07-19 17:27:23 +0900

    minor changes

--- model.py
+++ model.py
@@ -19,8 +19,8 @@ BATCH_SIZE=64
 embedding_mx = np.zeros(shape=(vocab_size, embedding_dim))
 
 with tf.name_scope("data"):
-    user = tf.keras.Input(shape=(history_length,))
-    item = tf.keras.Input(shape=(1,))
+    user_input = tf.keras.Input(shape=(history_length,))
+    item_input = tf.keras.Input(shape=(1,))
 
 with tf.name_scope("model"):
     # Sequencial Model
@@ -29,7 +29,7 @@ with tf.name_scope("model"):
                       enc_units=units,
                       batch_size=BATCH_SIZE,
                       embedding_mx=embedding_mx)
-    sample_output, sample_hidden = encoder(user)
+    sample_output, sample_hidden = encoder(user_input)
     # Attention layer
     attention_layer = Attention(units=10, history=history_length)
     attension_result, attention_weights = attention_layer(sample_hidden, sample_output)
@@ -38,7 +38,7 @@ with tf.name_scope("model"):
     user = tf.keras.layers.Dense(units=128, activation="relu")(attension_result)
     user = tf.keras.layers.Dense(units=64, activation="relu")(user)
     # item dense layer
-    item = encoder.embedding(item)
+    item = encoder.embedding(item_input)
     item = tf.keras.backend.squeeze(item, axis=1)
     item = tf.keras.layers.Dense(units=128, activation="relu")(item)
     item = tf.keras.layers.Dense(units=64, activation="relu")(item)
@@ -47,7 +47,7 @@ with tf.name_scope("model"):
     pred = tf.keras.layers.Activation(activation='sigmoid')(logit)
 
 with tf.name_scope("train"):
-    model = tf.keras.Model(inputs=[user, item], outputs=pred)
+    model = tf.keras.Model(inputs=[user_input, item_input], outputs=pred)
     filepath = "../data/checkpoints/model-{epoch:02d}-{val_acc:.2f}.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     model.compile(optimizer='adam',
